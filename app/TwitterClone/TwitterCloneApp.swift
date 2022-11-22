1d037af0b87425cbf405a681a905fcb84f0dbe03
danijel <danijel.kecman@gmail.com>
2022-11-22 04:22:24 +0100

    - init

---
+++ app/TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Danijel Kecman on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
